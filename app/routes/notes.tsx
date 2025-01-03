b2dad734b211090932a63e2a4aa998cba2ee4b3c
lizaandreevna2000 <karlsonzp2000@gmail.com>
2025-01-03 16:00:06 +0200

    add course design

---
+++ app/routes/notes.tsx
@@ -0,0 +1,14 @@
+import type { LinksFunction } from "@remix-run/node";
+import NewNote, { links as newNoteLinks} from "~/components/NewNote"
+
+export default function NotesPage() {
+    return (
+        <main>
+            <NewNote />
+        </main>
+    )
+}
+
+export const links: LinksFunction = () => [
+    ...newNoteLinks()
+  ];
