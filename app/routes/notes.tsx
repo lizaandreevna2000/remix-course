64cb5c69def6291134c94ff39705ad3ce80b72c0
lizaandreevna2000 <karlsonzp2000@gmail.com>
2025-01-06 11:17:00 +0200

    add loader and post data

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,14 +1,41 @@
-import type { LinksFunction } from "@remix-run/node";
+import type { LoaderFunctionArgs, ActionFunctionArgs, LinksFunction } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 import NewNote, { links as newNoteLinks} from "~/components/NewNote"
+import NoteLinks, { links as noteListLinks} from "~/components/NoteList"
+import { getStoredNotes, storeNotes } from "~/data/notesStoreData"
 
 export default function NotesPage() {
+    const notes = useLoaderData<typeof loader>();
     return (
         <main>
             <NewNote />
+            <NoteLinks notes={notes} />
         </main>
     )
 }
 
+export const loader = async ({
+  params,
+}: LoaderFunctionArgs) => {
+  const notes = await getStoredNotes();
+  return notes;
+}
+
+export const action = async ({ 
+    request 
+  }: ActionFunctionArgs) => {
+    const formData = await request.formData();
+    const noteData = Object.fromEntries(formData);
+
+    const existingNotes = await getStoredNotes();
+    noteData.id = new Date().toISOString();
+    const updatedNotes = existingNotes.concat(noteData);
+    await storeNotes(updatedNotes);
+    return redirect("/notes"); 
+  };
+
 export const links: LinksFunction = () => [
-    ...newNoteLinks()
+    ...newNoteLinks(), 
+    ...noteListLinks(),
   ];
