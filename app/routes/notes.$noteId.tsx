5edec3f7c3bf3164055636d0e7b48b4cc2c62fa4
lizaandreevna2000 <karlsonzp2000@gmail.com>
2025-01-09 11:44:46 +0200

    fix: dynamic route path and getting data with loader

--- app/routes/notes.$noteId.tsx
+++ app/routes/notes.$noteId.tsx
@@ -1,24 +1,38 @@
-import type { LinksFunction,  } from '@remix-run/node';
-import { Link } from '@remix-run/react';
+import { json } from "@remix-run/node";
+import type { LoaderFunctionArgs, ActionFunctionArgs, LinksFunction } from "@remix-run/node";
+import { Link, useLoaderData } from '@remix-run/react';
+import { getStoredNotes } from "~/data/notesStoreData";
+
 
 import styles from '~/styles/note-details.css'; 
 
 
+export const loader = async ({
+  params,
+}: LoaderFunctionArgs) => {
+  const notes = await getStoredNotes();
+  const noteId = params.noteId;
+  console.log(notes);
+  const selecteNote = notes?.find(note => note.id === noteId);
+  return selecteNote;
+};
+
 export default function NoteDetailsPage() {
+  const { note } = useLoaderData<typeof loader>(); 
   return (
     <main id="note-details">
       <header>
         <nav>
           <Link to="/notes">Back to all Notes</Link>
         </nav>
-        <h1>NOTE TITLE</h1>
+        <h1>{note?.title}</h1>
       </header>
-      <p id="note-details-content">NOTE CONTENT</p>
+      <p id="note-details-content">{note?.content}</p>
     </main>
   ); 
 }
  
 export const links: LinksFunction = () => [
     { rel: 'stylesheet', href: styles }
-  ];
+];
 
